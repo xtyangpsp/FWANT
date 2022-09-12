683449a46f4cf8b8b70213e3e1e23570317220dc
Xiaotao Yang <stcyang@gmail.com>
2022-09-12 17:34:30 -0400

    update

--- Project_template/tomography/ite_0.05deg_01/inv.structure.kerVpVs/FreqAll.Z.model/plt_slice.m
+++ Project_template/tomography/ite_0.05deg_01/inv.structure.kerVpVs/FreqAll.Z.model/plt_slice.m
@@ -1,24 +1,22 @@
 
 clear all;
 close all;
-
+addpath(genpath('/depot/xtyang/data/codes/mexcdf'))
+addpath(genpath('/depot/xtyang/data/codes/MatNoise'))
+addpath(genpath('/depot/xtyang/data/codes/FWANT/Codes/MatlabFiles'))
 MFILE_ROOT='../../../mfiles';
 path([MFILE_ROOT '/fun-spool'],path);
 path([MFILE_ROOT '/saclab'],path);
 
 % -- parameter --
-mx=69;my=159;mz=52;
-fnm_blk=['../block.' num2str(mx) 'x' num2str(my) 'x' num2str(mz) '.1x1x1.1x1x1.nc']
+mx=8;my=6;mz=8;
+fnm_blk=['../block.' num2str(mx) 'x' num2str(my) 'x' num2str(mz) '.1x1x1.1x1x1.nc'];
 
-%smot_list=[64];
-smot_list=[2 4 8 12 16 24];
-damp_list=[2 4 8 12 16 24];
-zval_list=[4 7 18 32 51 74 97 134 212];
-zindx_list=[51 50 47 44 42 40 38 35 29];
-%res_list={'./result.1th','./result.2th'};
+smot_list=[1.6];
+damp_list=[1.6];
+zindx_list=[8 7 6 5 4 3 2 1];
 res_list={'./result.1th'};
-%res_list={'./result.2th'};
-%res_list={'../Freq123.ENZ.dt.lt.10.model.wg1/result.1th','./result.2th'};
+
 cmp_list={'Vp','Vs'};
 
 savefig_flag=1;
@@ -32,13 +30,6 @@ num_res=numel(res_list);
 X=nc_varget(fnm_blk,'x');
 Y=nc_varget(fnm_blk,'y');
 Z=nc_varget(fnm_blk,'z');
-%x=reshape(X,[mx,my,mz])/1000;
-%y=reshape(Y,[mx,my,mz])/1000;
-%D=(-reshape(Z,[mx,my,mz])/1e3);
-
-%x=permute(x,[2,1,3]);
-%y=permute(y,[2,1,3]);
-%D=permute(D,[2,1,3]);
 
 x=90-reshape(X,[mx,my,mz])/pi*180;
 y=reshape(Y,[mx,my,mz])/pi*180;
@@ -51,85 +42,8 @@ Y=reshape(Y,[mx,my,mz]);
 [yc,xc,zc]=sph2cart(Y,pi/2-X,R);
 xc=xc/1e3; yc=yc/1e3; zc=zc/1e3; %in km
 
-minlon=-167.5; maxlon=-129.5; minlat=53.5;maxlat=67.5;
-%topo=qt;
-%Zin=topo;
-%lattopo=55-0.2*(13-1)-[1:524]*0.2+0.2; % id==2 start at 13th grid
-%lontopo=-30+0.2*(13-1)+[1:912]*0.2-0.2;
-% load /net/fs01/data/haiying/Proj/cascadia/misc/Topo_0.1deg.mat
-% topo=ELEV;
-% Zin=topo;
-% lattopo=TLAT;
-% lontopo=TLON;
-% flatmin=min(lattopo)*pi/180;flatmax=max(lattopo)*pi/180;
-% flonmin=min(lontopo)*pi/180;flonmax=max(lontopo)*pi/180;
-% flatmin=minlat*pi/180;flatmax=maxlat*pi/180;
-% flonmin=minlon*pi/180;flonmax=maxlon*pi/180;
-% phi_min=flatmin; phi_max=flatmax;
-% theta_min=flonmin; theta_max=flonmax;
-% Zin=double(Zin);theta_min=double(theta_min);theta_max=double(theta_max);
-% phi_min=double(phi_min);phi_max=double(phi_max);
-% cR=6371*1.e3;
-
-
-
-%{
-% plate boundaries
-!/bin/cp /home/yang/Proj/Shared/plate_boundaries/Bird_plate_boundaries.xy plate.dat
-load plate.dat
-plon=plate(:,1)*pi/180; plat=plate(:,2)*pi/180;
-pr(1:length(plon))=6371e3; pr=pr';
-nindplon=find(plon>flonmax | plon < flonmin);
-plon(nindplon)=[]; plat(nindplon)=[]; pr(nindplon)=[];
-nindplat=find(plat>flatmax | plat < flatmin);
-plon(nindplat)=[]; plat(nindplat)=[];pr(nindplat)=[];
-plon=plon*180/pi; plat=plat*180/pi;
-
-% hotspots (in lat, lon)
-%!/bin/cp /home/yang/Proj/Shared/hotspot_locations/hotspots.xy hotspot.dat
-!cat /home/yang/Proj/Shared/hotspot_locations/hotspot.lst | awk '{print $3, $2}' > hotspot.dat
-load hotspot.dat
-
-% coastline
-!cp /home/yang/Proj/Shared/coastline/coastline_1to5m.dat coast.dat
-load coast.dat
-coastlon=coast(:,1);coastlat=coast(:,2);
-%indx=find(coastlon < 137 | coastlon > 146);
-indx=find(coastlon < -27.6 | coastlon > 155);
-coastlon(indx)=[];coastlat(indx)=[];clear indx;
-indx=find(coastlat < -52 | coastlat > 52);
-coastlon(indx)=[];coastlat(indx)=[];clear indx
-clen=length(coastlat); cR(1:clen,1)=6381e3;
-[cy,cx,cz]=sph2cart(coastlon*pi/180,coastlat*pi/180,cR);
-%}
-
-%scl_xlim=[-250,250];
-%scl_ylim=[-200,200];
-
-%x2d=squeeze(x(:,:,end));
-%y2d=squeeze(y(:,:,end));
-%x1d=squeeze(x(:,1,end));
-%y1d=squeeze(y(1,:,end));
-%x1d=squeeze(x(1,:,end));
-%y1d=squeeze(y(:,1,end));
-
-%scl_xlim=[min(x1d)-1 max(x1d)+1];
-%scl_ylim=[min(y1d)-1 max(y1d)+1];
-%%%%%%%%%%%%%%%%%%%%%%%%%
-%% faults
-%%%%%%%%%%%%%%%%%%%%%%%%%
+minlon=min(y(1,:,1))-360; maxlon=max(y(1,:,1))-360; minlat=min(x(:,1,1));maxlat=max(x(:,1,1));
 %% stations
-%!cat /net/fs01/data/yang/n.cascadia/STinfo/sel_stns_cartcoord.lst | awk '{print $7,$8}' > st.lst
-%st=load('st.lst');
-%sxlon=[st(:,1)]/1000;
-%sylat=[st(:,2)]/1000;
-%!/bin/rm st.lst
-
-%!cp /net/fs01/data/yang/n.cascadia/STinfo/sel_stns_cartcoord.lst tmp.dat
-%[ntwk,stnm,slat,slon,sele,stny,stnx]= textread('tmp.dat','%s%s%f%f%f%f%f');
-%nst=length(stny);
-%stny=stny/1000;stnx=stnx/1000;
-%!/bin/rm tmp.dat
 
 % ----------------------------------
 %for nres=1:num_res
@@ -166,7 +80,7 @@ for ncmp=1:length(cmp_list)
     for nk=1:num_z
 %    for nk=8
         k=zindx_list(nk);
-        d=zval_list(nk);
+%         d=zval_list(nk);
         V=squeeze(Vel(:,:,k));
 		% remove the mean, 04/26/2011, Y.Shen
 		V=V-mean(mean(V));
@@ -174,145 +88,48 @@ for ncmp=1:length(cmp_list)
 	XS=squeeze(x(:,:,k));
 	YS=squeeze(y(:,:,k));
 	zs=squeeze(zc(:,:,k));
-        KDNM=num2str(zval_list(nk));
+%         KDNM=num2str(zval_list(nk));
         KINM=num2str(zindx_list(nk));
         Vmax=max(max(abs(V)));
 
-%        % shading interp
-%        hid=figure;set(gcf,'renderer','zbuffer');
-%        pcolor(x2d,y2d,V);
-%        shading interp
-%        colormap('jetwr');
-%        xlim(scl_xlim); ylim(scl_ylim);
-%        daspect([1,1,1]);
-%        hold on
-%        xlabel('x'); ylabel('y');
-%        colorbar
-%        title([' Depth ',num2str(D(1,1,k)),'km'])
-%        text(300, 250, cmp_list(ncmp))
-%     plot(fxlon,fylat, '-k')
-%     hold on
-%     plot(sxlon,sylat, '*k')
-
     if strcmp(KCMPNM, 'Vp')
     %fnm_fig=['figure_Vp_' int2str(nk) '.eps']
-    fnm_fig=['figure_Vp_s' num2str(smot_list(nsmot)) 'd' num2str(damp_list(ndamp)) '_z' KDNM];
+    fnm_fig=['figure_Vp_s' num2str(smot_list(nsmot)) 'd' num2str(damp_list(ndamp)) '_z' num2str(z(round(mx/2),round(my/2),k),2)];
     fnm_outfile=['Vp_' int2str(nk) '.txt'];
-    Vmax=3;
+    Vmax=10;
     end;
     if strcmp(KCMPNM, 'Vs')
-    fnm_fig=['figure_Vs_s' num2str(smot_list(nsmot)) 'd' num2str(damp_list(ndamp)) '_z' KDNM];
+    fnm_fig=['figure_Vs_s' num2str(smot_list(nsmot)) 'd' num2str(damp_list(ndamp)) '_z' num2str(z(round(mx/2),round(my/2),k),2)];
     fnm_outfile=['Vs_' int2str(nk) '.txt'];
-    Vmax=3;
+    Vmax=10;
     end;
 
-%        caxis([-Vmax,Vmax]);
-
-%        Vmax=max(max(abs(V)));
-
-hid=figure('Position',[400 400 700 700]); hold on, box on, axis on
+hid=figure('Position',[400 -200 700 700]); hold on, box on, axis on
 set(hid,'renderer','zbuffer');
 set(gca,'TickDir','out');
 
-%       imagesc(x1d,y1d,V);hold on
-%	imagesc(y1d,x1d,V);hold on
-pcolor(YS,XS,V);
-%        surf(squeeze(permute(ys,[2 1 3])), ...
-%            squeeze(permute(xs,[2 1 3])), ...
-%            squeeze(permute(zs,[2 1 3])), ...
-%            squeeze(permute(V,[2 1 3])));hold on
-%sphere3d1c(Zin,theta_min,theta_max,phi_min,phi_max,cR,1,'contour','nearest',0);
-%view(-10,5);
-%axis off; axis tight;
+pcolor(YS-360,XS,V);
 
-%contour(lontopo,lattopo,topo,[1 1],'Color','k');
-%contour(lontopo,lattopo,topo,[10 10],'Color',[0.2 0.8 1.0]);hold on
-%contour(lontopo,lattopo,topo,[2000 2000],'Color',[0.8 0.4 0.2]);hold on
-
-% [clon,clat]=textread('/net/fs01/data/haiying/Proj/cascadia/misc/PNWcoast_revised.dat','%f %f');
-% plot(clon+360,clat,'k.','MarkerSize',2);
-% 
-% state=[];
-% load /net/fs01/data/haiying/Proj/cascadia/misc/us_states.mat
-% for sb=1:length(state)
-%     plot(state(sb).polygon(:,1)+360, state(sb).polygon(:,2),'color','k','LineWidth',1);
-% end
-
-% 	plot(plon,plat,'+','MarkerSize',1,'Color',[0.2 0.2 0.2]);hold on
-% 	plot(hotspot(:,2),hotspot(:,1),'ko','MarkerSize',5,'LineWidth',1); hold on
-%	contour(x1d,y1d,D(:,:,end),[-1.0 -0.5 0],'Color',[0 1 0]);hold on
-%	contour(x1d,y1d,D(:,:,end),[-1.5 -1.5],'Color',[1 0 0]); hold on
-%	for ii=1:nst
-%	plot(stnx(ii),stny(ii),'k^'); hold on
-%	end
 shading flat
 colormap('jetwr');
-%colormap('jet');
 
-       % xlim(scl_xlim); ylim(scl_ylim);
 daspect([1,cosd((minlat+maxlat)/2),1]);
 xlabel('Longitude, deg.'); ylabel('Latitude, deg.');
 axis([minlon maxlon minlat maxlat]);
 colorbar
-%        shading interp
-%        colormap('jetwr');
-       % xlim(scl_xlim); ylim(scl_ylim);
-%        daspect([1,1,1]);
-%        hold on
-%        xlabel('x'); ylabel('y');
-%        colorbar
-%        set(gca,'ydir','normal');
+
 title(['smooth: ' num2str(smot_list(nsmot)) ', damp: ' num2str(damp_list(ndamp)) ','...
-    cmp_list{ncmp} ', z: ',num2str(z(round(mx/2),round(my/2),k),4),' km'])
+    cmp_list{ncmp} ', z: ',num2str(z(round(mx/2),round(my/2),k),2),' km'])
 caxis([-Vmax,Vmax]);
-%      caxis([-Vmax,Vmax]*100);
 
-%cR=6371*1.e3;
-%sphere3d(Zin,theta_min,theta_max,phi_min,phi_max,cR,4.0*pi/180,'contour','spline',0);
-%Zin=double(Zin);theta_min=double(theta_min);theta_max=double(theta_max);
-%phi_min=double(phi_min);phi_max=double(phi_max);
-%sphere3d1c(Zin,theta_min,theta_max,phi_min,phi_max,cR,1,'contour','nearest',0);
-%view(-10,5);
-%axis off; axis tight;
-
-%     plot(fxlon,fylat, '-k')
-%     hold on
-%     plot(sxlon,sylat, '*k')
-
-%     print('-depsc',fnm_fig);
 if savefig_flag
     print('-dpng',['./' pnm_fig '/' fnm_fig '.png']);
     close;
 end
 
-%minlon=-124.85; maxlon=-118.75; minlat=45.5;  maxlat=49.3;
-% center of the xy coordinates in latitude and longitude
-%lat0=(minlat+maxlat)/2; lon0=(minlon+maxlon)/2; 
-%x0=0;y0=0;alpha=90;
-%fid=fopen(char(fnm_outfile),'w+');
-%kk=1;
-%for ii=1:length(x1d)
-%for jj=1:length(y1d)
-%x=x1d(ii)*1000;
-%y=y1d(jj)*1000;
-%[xlat,ylon]=cart2geo(x,y,x0,y0,lat0,lon0,alpha);
-%fprintf(fid,'%f %f %f\n',xlat,ylon,V(kk));
-%kk=kk+1;
-%end
-%end
-%fclose(fid);
-
     end
 end
-%close all
 
 end
 end
 end
-
-%plot([55 55],[23,43],'k')
-%plot([50.2 50.2],[23,43],'k')
-%plot([40, 65],[37.1,37.1],'k')
-%plot([40, 65],[29.9,29.9],'k')
-
-
